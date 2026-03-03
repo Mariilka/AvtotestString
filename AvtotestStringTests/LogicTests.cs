77ec9d16c8c771ed72eef136ace146717d391236
Marina <aminovamarina2006@gmail.com>
2026-03-03 16:23:26 +0800

    Начинаю создавать автотесты

---
+++ AvtotestStringTests/LogicTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Three;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Three.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void CalculateLetterPercentageTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
