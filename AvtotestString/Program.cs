19e76a1343806c5da2c5f8ee7aec6904d2a112ed
Marina <aminovamarina2006@gmail.com>
2026-03-03 16:21:49 +0800

    Написала 3 задачус декомпозцией

--- AvtotestString/Program.cs
+++ AvtotestString/Program.cs
@@ -1,15 +1,50 @@
-﻿eddeusing System;
+﻿//Дано предложение. Ограничить долю (в %) буквой нем.
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AvtotestString
+namespace Three
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Logic textAnalyzer = new Logic();
+
+
+            while (true)
+            {
+                Console.WriteLine("\nВведите предложение (или 'exit' для выхода):");
+                string sentence = Console.ReadLine();
+
+                if (sentence?.ToLower() == "exit")
+                    break;
+
+                // Используем метод из класса Logic
+                double letterPercentage = textAnalyzer.CalculateLetterPercentage(sentence);
+
+                Console.WriteLine($"Предложение: \"{sentence}\"");
+                Console.WriteLine($"Доля букв: {letterPercentage:F2}%");
+            }
+
+
+
         }
     }
+    public class Logic
+    {
+        public double CalculateLetterPercentage(string text)
+        {
+            if (string.IsNullOrEmpty(text)) // является ли строка пустой
+                return 0;
+
+            int letterCount = text.Count(char.IsLetter);// считает кол-во элементов
+            int totalChars = text.Length;
+
+            return (double)letterCount / totalChars * 100;
+        }
+    }
+
 }
